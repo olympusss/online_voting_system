ab1d3b0cc42e31bc269dfc2f09b34ccf00f7933a
olympusss <rasulov.titu@gmail.com>
2023-03-06 04:20:01 +0500

    commit

--- e_voting/settings.py
+++ e_voting/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.admin',
     'django.contrib.auth',
