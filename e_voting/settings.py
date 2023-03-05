4177ca21d2ab7fc677789a17121b2552a0a7fa53
olympusss <rasulov.titu@gmail.com>
2023-03-06 04:47:19 +0500

    commit

--- e_voting/settings.py
+++ e_voting/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     #   You can use this :
         'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
