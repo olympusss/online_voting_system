f0139ceb40f0aa16619e564e496cb8273710eaed
olympusss <rasulov.titu@gmail.com>
2023-03-06 04:15:26 +0500

    commit

--- e_voting/settings.py
+++ e_voting/settings.py
@@ -128,9 +128,9 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 AUTH_USER_MODEL = 'account.CustomUser'
 AUTHENTICATION_BACKENDS = ['account.email_backend.EmailBackend']
