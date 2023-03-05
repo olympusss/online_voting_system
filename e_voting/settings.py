e1eb5b0d56ee03076a08f44e2cede2bbf7d2e7ff
olympusss <rasulov.titu@gmail.com>
2023-03-06 04:48:20 +0500

    commit

--- e_voting/settings.py
+++ e_voting/settings.py
@@ -13,9 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_DIR=os.path.join(BASE_DIR,'static')
-MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -83,7 +81,7 @@ DATABASES = {
     #   You can use this :
         'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -124,16 +122,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS=[
-    STATIC_DIR,
-]
+MEDIA_ROOT = '/home/ubuntu/project/online_voting_system/media/'
+STATIC_ROOT = '/home/ubuntu/project/online_voting_system/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 AUTH_USER_MODEL = 'account.CustomUser'
 AUTHENTICATION_BACKENDS = ['account.email_backend.EmailBackend']
