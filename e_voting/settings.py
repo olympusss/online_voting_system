00195b420ca98d08b25b2107d1b7ed4b2b760a45
olympusss <rasulov.titu@gmail.com>
2023-03-06 05:16:09 +0500

    commit

--- e_voting/settings.py
+++ e_voting/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "/media/")
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 # STATICFILES_DIRS = [
